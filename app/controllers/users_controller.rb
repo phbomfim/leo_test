254cee2be67d11a3215a0288ace04ad4c3e82ce5
Leonardo Fragozo <lqfragozo@gmail.com>
2021-12-23 16:44:03 -0300

    creates the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
-class UsersController
-
+# frozen_string_literal: true
+# Creates the users controller
+class UsersController < ApplicationController
   def new
     @user = User.new
   end
 
+  def create
+    @user = User.create(params.require(:user).call(:email, :name, :age))
+    if @user.valid?
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
 end
