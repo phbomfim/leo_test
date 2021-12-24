5a0dd400353395f3502d0a9d2727a9569fffadb5
Leonardo Fragozo <lqfragozo@gmail.com>
2021-12-24 12:01:21 -0300

    morning 24-12 first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,23 @@
 # frozen_string_literal: true
+
 # Creates the users controller
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.create(params.require(:user).call(:email, :name, :age))
-    if @user.valid?
-      redirect_to users_path
+    @user = User.new(email: ''..'', name: ''...'', age: ''...'')
+    if @user.save
+      redirect_to @user
     else
       render :new
     end
