994cc4e017b29ecefa9d87370507e05a03ec3fff
Leonardo Fragozo <lqfragozo@gmail.com>
2021-12-27 21:43:18 -0300

    improving the crud monday night

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,11 +15,38 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(email: ''..'', name: ''...'', age: ''...'')
+    @user = User.new(user_params)
     if @user.save
       redirect_to @user
     else
       render :new
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :age)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  validates :email, :presence => true
-  validates :name, :presence => true
-  validates :age, comparasion: { greater_than: 18 }
-
-
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :age, presence: true
 end
