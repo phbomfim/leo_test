be8941bc27512305e52e8ab8b3d1b166cefd461f
Leonardo Fragozo <lqfragozo@gmail.com>
2022-01-03 09:27:25 -0300

    still facing errors v2

--- spec/features/_form_spec.rb
+++ spec/features/_form_spec.rb
@@ -4,11 +4,10 @@ require 'rails_helper'
 
 RSpec.feature '_form', type: :feature do
   context 'filling the form' do
-    scenario 'filling the user crud' do
+    scenario 'filling the user elements' do
       visit users_path
-      fill_in :name, with: 'Leo'
+      fill_in 'name', with: 'Leo'
       expect(page).to have_field('name', with: 'Leo')
-
     end
   end
 end
