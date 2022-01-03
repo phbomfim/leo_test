24709282e548d66c1d27a93e78feef4a33c41ac3
Leonardo Fragozo <lqfragozo@gmail.com>
2022-01-03 08:56:17 -0300

    still facing errors

--- spec/features/_form_spec.rb
+++ spec/features/_form_spec.rb
@@ -7,11 +7,8 @@ RSpec.feature '_form', type: :feature do
     scenario 'filling the user crud' do
       visit users_path
       fill_in :name, with: 'Leo'
-      expect(page).to_have_field(:name, with: 'Leo')
-      fill_in :email, with: 'lqfragozo@gmail.com'
-      expect(page).to_have_field(:name, with: 'lqfragozo@gmail.com')
-      fill_in :age, with: '28'
-      expect(page).to_have_field(:name, with: '28')
+      expect(page).to have_field('name', with: 'Leo')
+
     end
   end
 end
