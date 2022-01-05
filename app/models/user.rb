129c50c6311118bfbaf19bb9c67df46928e597fa
Leonardo Fragozo <lqfragozo@gmail.com>
2022-01-05 11:26:38 -0300

    add email and name cant be the same validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :name, presence: true
   validates :age, presence: true, numericality: { greater_than_or_equal_to: 18 }
+  validate :check_email_and_name
+
+  def check_email_and_name
+    errors.add(:email, "can't be the same as name") if name == email
+  end
 end
