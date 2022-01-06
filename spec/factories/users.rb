9456df4768f3cfbe56d7688b26d8d2f30cb29f06
Leonardo Fragozo <lqfragozo@gmail.com>
2022-01-06 12:35:12 -0300

    add create specs to model user

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :user do
+    name { 'Leo' }
+    email { 'lqfragozo@gmail.com' }
+    age { '28' }
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context 'validations' do
-    it { is_expected.to validate_presence_of :name }
-    it { is_expected.to validate_presence_of :email }
-    it { is_expected.to validate_presence_of :age }
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_presence_of :email }
+  it { is_expected.to validate_presence_of :age }
+
+  let(:user) { build(:user) }
+  describe '#create' do
+    it {
+      expect(user.name).to(eq('Leo'))
+      expect(user.email).to(eq('lqfragozo@gmail.com'))
+      expect(user.age).to(eq(28))
+    }
   end
 end
