abb1b1ffd1f87a054bdad84198c24852e5a67b26
Leonardo Fragozo <lqfragozo@gmail.com>
2022-01-06 14:28:01 -0300

    add email validation, and specs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: 'only allows letters' }, uniqueness: true
   validates :age, presence: true, numericality: { greater_than_or_equal_to: 18 }
   validate :check_email_and_name
