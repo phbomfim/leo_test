c7d50787cc019b559b8092626bbdf6f018577bd1
Leonardo Fragozo <lqfragozo@gmail.com>
2022-01-11 19:20:58 -0300

    rubocop fix

--- spec/features/_form_spec.rb
+++ spec/features/_form_spec.rb
@@ -3,21 +3,20 @@
 require 'rails_helper'
 
 RSpec.feature '_form', type: :feature do
-    it 'has a form with name, email and age' do
-      visit new_user_path
-      expect(page).to have_field(:Name)
-      expect(page).to have_field(:Email)
-      expect(page).to have_field(:Age)
-    end
-
-    it 'can fill in a form with information in the right page' do
-      visit new_user_path
-
-      fill_in :Name, with: 'Leonardo'
-      fill_in :Email, with: 'teste@test.com'
-      fill_in :Age, with: '28'
-
-      expect(page).to have_current_path(new_user_path)
-    end
+  it 'has a form with name, email and age' do
+    visit new_user_path
+    expect(page).to have_field(:Name)
+    expect(page).to have_field(:Email)
+    expect(page).to have_field(:Age)
   end
 
+  it 'can fill in a form with information in the right page' do
+    visit new_user_path
+
+    fill_in :Name, with: 'Leonardo'
+    fill_in :Email, with: 'teste@test.com'
+    fill_in :Age, with: '28'
+
+    expect(page).to have_current_path(new_user_path)
+  end
+end
