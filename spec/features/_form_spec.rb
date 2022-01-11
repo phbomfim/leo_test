4ad7cb3d8f3593ca6a4551432226cd18e53e6064
Leonardo Fragozo <lqfragozo@gmail.com>
2022-01-11 19:19:15 -0300

    capybara specs fix

--- spec/features/_form_spec.rb
+++ spec/features/_form_spec.rb
@@ -3,11 +3,21 @@
 require 'rails_helper'
 
 RSpec.feature '_form', type: :feature do
-  context 'filling the form' do
-    scenario 'filling the user elements' do
-      visit users_path
-      fill_in 'name', with: 'Leo'
-      expect(page).to have_field('name', with: 'Leo')
+    it 'has a form with name, email and age' do
+      visit new_user_path
+      expect(page).to have_field(:Name)
+      expect(page).to have_field(:Email)
+      expect(page).to have_field(:Age)
+    end
+
+    it 'can fill in a form with information in the right page' do
+      visit new_user_path
+
+      fill_in :Name, with: 'Leonardo'
+      fill_in :Email, with: 'teste@test.com'
+      fill_in :Age, with: '28'
+
+      expect(page).to have_current_path(new_user_path)
     end
   end
-end
+
