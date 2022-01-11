bdd8d7813d9681d9c13336b2a3d95860de353111
Leonardo Fragozo <lqfragozo@gmail.com>
2022-01-11 10:50:31 -0300

    convert db to json rails seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+File.open("user.json", "w") { |f| f.write User.all.to_json }
